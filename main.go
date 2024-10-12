6ed7cbef8aee0c2b96993e7080d2a3a8e3c9723f
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-10-12 16:53:21 +0530

    moved to project-planton

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	locustkubernetesv1 "buf.build/gen/go/plantoncloud/project-planton/protocolbuffers/go/project/planton/provider/kubernetes/locustkubernetes/v1"
+	locustkubernetesv1 "buf.build/gen/go/project-planton/apis/protocolbuffers/go/project/planton/provider/kubernetes/locustkubernetes/v1"
 	"github.com/pkg/errors"
-	"github.com/plantoncloud/locust-kubernetes-pulumi-module/pkg"
-	"github.com/plantoncloud/pulumi-module-golang-commons/pkg/stackinput"
+	"github.com/project-planton/locust-kubernetes-pulumi-module/pkg"
+	"github.com/project-planton/pulumi-module-golang-commons/pkg/stackinput"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
